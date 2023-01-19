0a117a5a8da7a66a22b7d7376a2cbdb74c60d498
paulportilla1 <116098987+Paulportilla1@users.noreply.github.com>
2023-01-19 16:38:31 -0500

    Add files via upload

---
+++ deber de array en javascript/reprueban.js
@@ -0,0 +1,8 @@
+let ratings = [60, 85, 75, 90, 55, 80];
+for (let i = 0; i < ratings.length; i++) {
+    if (ratings[i] >= 70) {
+        console.log(ratings[i]+"aprueba");
+    } else {
+        console.log(ratings[i]+"reprueba");
+    }
+}
